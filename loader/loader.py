807a9b1da6796d7dace6b335518760694f6d3975
koshka27 <anechka__7@mail.ru>
2022-04-20 00:49:23 +0800

    .

---
+++ loader/loader.py
@@ -0,0 +1,37 @@
+import logging
+
+from flask import Blueprint, request, render_template
+from functions import load_posts, uploads_posts
+
+loader_blueprint = Blueprint("loader", __name__, url_prefix="/post", static_folder="static", template_folder="templates")
+
+
+@loader_blueprint.route('/form/')
+def form():
+    return render_template("post_form.html")
+
+
+@loader_blueprint.route('/upload/', methods=["POST"])
+def upload():
+    try:
+        file = request.files["picture"]
+        filename = file.filename
+        content = request.values["content"]
+        posts = load_posts()
+        posts.append({
+            "pic": f"/uploads/images/{filename}",
+            "content": content
+        })
+        uploads_posts(posts)
+        file.save(f"uploads/images/{filename}")
+        if filename.split(".")[-1] not in ["png", "jpeg", "jpg"]:
+            logging.info("Файл не изображение")
+    except FileNotFoundError:
+        logging.error("Ошибка при загрузке файла")
+        return "<h1> Файл не найден </h1>"
+    else:
+        return render_template("post_uploaded.html", pic=f"/uploads/images/{filename}", content=content)
+
+
+    # posts = [x for x in load_posts() if search_by.lower() in x["content"].lower()]
+    # return render_template("post_list.html", search_by=search_by, posts=posts)
